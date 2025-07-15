38c9bed3aef63eb8f52e6f80e7fd2c688d060cc1
Ashwin116T <ashwin25novak@gmail.com>
2025-07-14 23:51:50 -0400

    notebook file

--- code/monocular_reconstruction.py
+++ code/monocular_reconstruction.py
@@ -4,3 +4,8 @@ from matplotlib import pyplot as plt
 from PIL import Image
 import torch
 from transformers import GLPNImageProcessor, GLPNForDepthEstimation
+
+
+feature_extractor = GLPNImageProcessor.from_pretrained("vinvino02/glpn-nyu")
+model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
+
