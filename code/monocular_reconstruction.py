c225c9433786fc7cb8bf066c99463a49cb69f74a
Ashwin116T <ashwin25novak@gmail.com>
2025-07-14 23:02:29 -0400

    added a new file

--- code/monocular_reconstruction.py
+++ code/monocular_reconstruction.py
@@ -3,4 +3,4 @@ matplotlib.use('TkAgg')  # Use TkAgg backend for matplotlib
 from matplotlib import pyplot as plt
 from PIL import Image
 import torch
-from transformers import GLPN
+from transformers import GLPNImageProcessor, GLPNForDepthEstimation
