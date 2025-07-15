146ef908483f203976207f8cad487a496fcd6e2b
Ashwin116T <ashwin25novak@gmail.com>
2025-07-14 23:01:34 -0400

    initial commit

---
+++ code/monocular_reconstruction.py
@@ -0,0 +1,6 @@
+import matplotlib
+matplotlib.use('TkAgg')  # Use TkAgg backend for matplotlib
+from matplotlib import pyplot as plt
+from PIL import Image
+import torch
+from transformers import GLPN
